bcebc1b263fb94a6275a9f6a0e618910cf4f0c61
cristianiniguez <felizpc1@gmail.com>
2020-09-18 16:04:26 -0400

    Implementando Boom

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ const { config } = require('./config/index')
 
 const moviesApi = require('./routes/movies')
 
-const { logErrors, errorhandler } = require('./utils/middleware/errorHandlers')
+const { logErrors, errorhandler, wrapErrors } = require('./utils/middleware/errorHandlers')
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler')
 
 // body parser
 app.use(express.json())
 
+// routes
 moviesApi(app)
 
+// Catch 404
+app.use(notFoundHandler)
+
+// Errors middleware
 app.use(logErrors)
+app.use(wrapErrors)
 app.use(errorhandler)
 
 app.listen(config.port, function () {
